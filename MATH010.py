4259ce283f7a835197e602296edaf536d8586953
hillel <hillel.weber@gmail.com>
2021-07-02 19:29:35 -0700

    first python pr

--- MATH010.py
+++ MATH010.py
@@ -1,3 +1,8 @@
-num=4567
+num=9485
+a=num//1000
+h=(num-(a*1000))//100
+t=(num-(a*1000+h*100))//10
+o=(num-(a*1000+h*100+t*10))
+print("Alafim: " + str(a) + "\nMeot: " + str(h) + "\nAsarot: "+ str(t) + "\nAhadot: " + str(o))
 
-print("Alafim: " + str(num//1000) + "\nMeot: " + str((num-4000)//100) + "\nAsarot: "+ str((num-4500)//10) + "\nAhadot: " + str((num-4560)//1))
+print("Alafim: " + str(num//1000) + "\nMeot: " + str(num%1000//100) + "\nAsarot: "+ str(num%100//10) + "\nAhadot: " + str(num%10))
