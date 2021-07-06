dc67404238320826095d15ad548cdd63468adeb9
hillel <hillel.weber@gmail.com>
2021-07-05 22:58:00 -0700

    first python pr

---
+++ STRING TAR01.py
@@ -0,0 +1,19 @@
+# print("p1={},p2={},p3={},p4={red}".format(1,1.0,"HW",red="BBFD"))
+
+#s1="abcde"
+#print(s1[1])
+#print(s1[-1])
+#print(s1[1:-3])
+#print(s1[-1:6])
+
+
+name=("hillel weber")
+age=57
+sstring="ayelet hillel weber yael noa rachel ruth tamy ido"
+
+print("My bame is: " + name + "\nMy age is : "+str(age))
+print("My bame is: " + name + "\nMy age *3 is : "+str(age*3))
+print("my name in reverse is:" + name[::-1])
+print("hillel" in sstring)
+print(name in sstring)
+print(name[3::11] in sstring)
