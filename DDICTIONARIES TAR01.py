a21af85e37b046c2947bdb7e2d6b498799095efe
hillel <hillel.weber@gmail.com>
2021-07-15 22:52:50 -0700

    first python pr

--- DDICTIONARIES TAR01.py
+++ DDICTIONARIES TAR01.py
@@ -1,5 +1,5 @@
 #dictionaries
-
+'''
 adict={"www.google.com":"8.8.8.8","www.facebook.com":"7.7.7.7","www.youtube.com":["5.5.5.5","4.4.4.4"]}
 bdict=dict()
 cdict={"www.cccc.com":"9.9.9.9","wwww.dddd.com":"11.11.11.11"}
@@ -20,3 +20,15 @@ print(adict.values())
 
 print("www.googl.com" in adict)
 print("7.8.7.7" in adict.values())
+'''
+
+#LAB05
+
+tdict={"Ron Smith":"1000","James Jones":"32","Martha Viniard":"28","Brandy Car":"40","Jonah Brother":"35"}
+print(tdict)
+a=int(tdict["Ron Smith"]) + int(tdict["Jonah Brother"])
+tdict.update({"eric clap": str(a)})
+print("this is the sum of money: " + str(a))
+print(tdict)
+print(str(len(tdict)))
+print("rosanne" in tdict)
