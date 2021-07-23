0d50497ac45b97c163a146788b0adbf0547865f4
hillel <hillel.weber@gmail.com>
2021-07-23 08:17:58 -0700

    first python pr

--- LESSON 02/files01.py
+++ LESSON 02/files01.py
@@ -1,17 +1,26 @@
 #files01
-
+'''
 filename = "C:/Users/hille/Desktop/Documents/HILLEL/CYBER COURSE/PYTHON/handling.txt"
 file = open(filename, "r+")
-file.write("crazy people\n")
+file.write("let's go\n")
 print(file.read())
 file.close()
 
 
 #append
 file = open(filename, "a")
-file.write("crazy people\n")
+file.write("crazy people\n192.168.0.1\n")
 
 #print to screen
 file = open(filename, "r")
 print(file.read())
 file.close()
+
+#files02
+filename = "C:/Users/hille/Desktop/Documents/HILLEL/CYBER COURSE/PYTHON/handling.txt"
+file = open(filename, "r")
+for line in file:
+    print(line)
+file.close()
+'''
+
